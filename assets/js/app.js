4f518d738e934c18275b545ac83d7005a2a58840
Nick Tan <nctan1002@gmail.com>
2018-06-03 17:31:18 -0400

    Moved ajax calls to a function and call function on load on user's location

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,72 +5,17 @@ $(document).ready(function () {
         method: "GET"
     }).then(function (response) {
         city = response.city;
+        params["q"] = city;
         console.log(city);
-        // code to query weather api goes here for testing purposes
-
-
+        getWeatherData(city);
     });
 
     $('#submitWeather').click(function (event) {
         event.preventDefault();
-        city = $('#city').val();
-
-        params["q"] = city;
-
-        let currentQ = currentUrl + $.param(params);
-        let forecastQ = forecastUrl + $.param(params);
-
-        // get current weather
-        $.ajax({
-            url: currentQ,
-            method: "GET"
-        }).then(function (res) {
-            let weather = {
-                sunrise: moment(res.sys.sunrise).format("hh:mm a"),
-                sunset: moment(res.sys.sunset).format("hh:mm a"),
-                description: res.weather[0].description, // more precise description e.g. light rain
-                icon: res.weather[0].icon,
-                main: res.weather[0].main, // main weather e.g rain
-                wind: {
-                    deg: res.wind.deg,
-                    speed: res.wind.speed,
-                }
-            };
-            current = res.main;
-            for (let prop in weather) {
-                current[prop] = weather[prop];
-            }
-            console.log(current);
-        });
-
-        // get forecast weather
-        $.ajax({
-            url: forecastQ,
-            method: "GET"
-        }).then(function (res) {
-            for (let i = 0; i < res.list.length; i++) {
-                data = res.list[i];
-                let weather = {
-                    dt: data.dt_txt,
-                    description: data.weather[0].description,
-                    icon: data.weather[0].icon,
-                    main: data.weather[0].main,
-                    wind: {
-                        deg: data.wind.deg,
-                        speed: data.wind.speed,
-                    },
-                    rain: data.rain, // rain volume for last three hours
-                    humidity: data.main.humidity,
-                    pressure: data.main.pressure,
-                    temp: data.main.temp,
-                    temp_max: data.main.temp_max,
-                    temp_min: data.main.temp_min,
-                };
-                forecast.push(weather);
-            }
-            console.log(forecast);
-        });
-
+        if ($('#city').val() !== "") {
+            city = $('#city').val();
+        }
+        getWeatherData(city);
         $('#formId')[0].reset();
     });
 
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,21 +1,57 @@
-// get city
-function getCityData(city="") {
-    var data;
-    if (!city) {
-        $.ajax({
-            url: "https://api.ipdata.co",
-            method: "GET"
-        }).then(function(response) {
-            city = response.city;
-            data = getData(city);
-        });
-    } else {
-        data = getData(city);
-        return data;
-    }
-}
+// get weather data
+function getWeatherData(city) {
+    params["q"] = city;
+    let currentQ = currentUrl + $.param(params);
+    let forecastQ = forecastUrl + $.param(params);
 
-// query openweather api - Artur's code goes below
-function getData(city) {
-    
+    // get current weather
+    $.ajax({
+        url: currentQ,
+        method: "GET"
+    }).then(function (res) {
+        let weather = {
+            sunrise: moment(res.sys.sunrise).format("hh:mm a"),
+            sunset: moment(res.sys.sunset).format("hh:mm a"),
+            description: res.weather[0].description, // more precise description e.g. light rain
+            icon: res.weather[0].icon,
+            main: res.weather[0].main, // main weather e.g rain
+            wind: {
+                deg: res.wind.deg,
+                speed: res.wind.speed,
+            }
+        };
+        current = res.main;
+        for (let prop in weather) {
+            current[prop] = weather[prop];
+        }
+        console.log(current);
+    });
+
+    // get forecast weather
+    $.ajax({
+        url: forecastQ,
+        method: "GET"
+    }).then(function (res) {
+        for (let i = 0; i < res.list.length; i++) {
+            data = res.list[i];
+            let weather = {
+                dt: data.dt_txt,
+                description: data.weather[0].description,
+                icon: data.weather[0].icon,
+                main: data.weather[0].main,
+                wind: {
+                    deg: data.wind.deg,
+                    speed: data.wind.speed,
+                },
+                rain: data.rain, // rain volume for last three hours
+                humidity: data.main.humidity,
+                pressure: data.main.pressure,
+                temp: data.main.temp,
+                temp_max: data.main.temp_max,
+                temp_min: data.main.temp_min,
+            };
+            forecast.push(weather);
+        }
+        console.log(forecast);
+    });
 }
